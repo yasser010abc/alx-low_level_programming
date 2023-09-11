53dca96226ef8a78638302e282e8ccaaa196f20b
yasser010abc <yassirbenhouria2000@gmail.com>
2023-09-11 10:35:39 -0700

    a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,19 +4,19 @@
  * main - Entry point
  *
  * Description: print all possible different
- *	     combinations of tow digits.
+ *	     combinations of two digits.
  *
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	int digit = 0, digit2;
+	int digit1 = 0, digit2;
 
 	while (digit1 <= 9)
 	{
 		digit2 = 0;
-		while (digit <= 9)
+		while (digit2 <= 9)
 		{
 			if (digit1 != digit2 && digit1 < digit2)
 			{
