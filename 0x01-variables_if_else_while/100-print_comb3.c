1f61baa46e74b53ed5e729f4b70d0ced9af8dc59
yasser010abc <yassirbenhouria2000@gmail.com>
2023-09-11 10:29:49 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different
+ *	     combinations of tow digits.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0, digit2;
+
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+		while (digit <= 9)
+		{
+			if (digit1 != digit2 && digit1 < digit2)
+			{
+				putchar(digit1 + 48);
+				putchar(digit2 + 48);
+
+				if (digit1 + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		digit2++;
+	}
+	putchar('\n');
+
+	return (0);
+}
