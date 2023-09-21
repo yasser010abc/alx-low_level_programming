6363d3fd93b42bcdba7b00d360b85d4b9fe82f85
yasser010abc <yassirbenhouria2000@gmail.com>
2023-09-21 06:45:34 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,7 +21,7 @@ int isLower(char c)
 int isDelimiter(char c)
 {
 	int i;
-	char delimiter[] = " \t\n,.!?\"(){}";
+	char delimiter[] = " \t\n,.!?\"()[}";
 
 	for (i = 0; i < 12; i++)
 		if (c == delimiter[i])
