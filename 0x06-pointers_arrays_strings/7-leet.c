1d006fb91223201638780373316dcf8ab236e3f6
yasser010abc <yassirbenhouria2000@gmail.com>
2023-09-21 07:17:07 -0700

    a

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,10 +15,10 @@ char *leet(char *c)
 
 	while (*c)
 	{
-		for (i = 0, i < sizeof(key) / sizeof(char); i++)
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
 		{
-			/*32 is the difference between lower case letters and upper case letters*/
-			if (*c == key[] || *c == key[i] + 32)
+			/*32 is the difference between lowercase letters and uppercase letters*/
+			if (*c == key[i] || *c == key[i] + 32)
 			{
 				*c = 48 + value[i];
 			}
