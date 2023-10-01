457adddbe71e8c4becf6d3c8b20dd7f4c89685e1
yasser010abc <yassirbenhouria2000@gmail.com>
2023-10-01 16:56:27 -0700

    a

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -25,6 +25,25 @@ int inception(char *s1, char *s2)
 {
 	int ret = 0;
 
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
+
+/**
+ * wildcmp - compares two strings lexicographically
+ * @s1: the first string
+ * @s2: the second string, can contain wildcard
+ *
+ * Return: 1 if identical, 0 if false
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
 	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
 		return (1);
 	if (*s1 == *s2)
